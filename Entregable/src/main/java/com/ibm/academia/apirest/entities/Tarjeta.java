e022552a05d60016a3deaabf01e6936f36705b35
Braulio <braulio.garrido2209@gmail.com>
2021-11-29 09:20:44 -0600

    commit

--- Entregable/src/main/java/com/ibm/academia/apirest/entities/Tarjeta.java
+++ Entregable/src/main/java/com/ibm/academia/apirest/entities/Tarjeta.java
@@ -4,12 +4,25 @@ import java.io.Serializable;
 
 import com.ibm.academia.apirest.enums.TipoTarjeta;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tarjeta implements Serializable {
 
 	private TipoTarjeta tipoTarjeta;
 	
-	
+	@Override
+	public String toString() {
+		return "Tarjeta [tipoTarjeta=" + tipoTarjeta + "]";
+	}
+
+
 	private static final long serialVersionUID = 2911762464333103201L;
 
 }
