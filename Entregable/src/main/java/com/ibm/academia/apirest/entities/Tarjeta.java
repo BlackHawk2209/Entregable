678ba90e0ab97a0c3ac7bb7e2456f66a91955e9d
Braulio <braulio.garrido2209@gmail.com>
2021-11-29 09:08:07 -0600

    primer commit

---
+++ Entregable/src/main/java/com/ibm/academia/apirest/entities/Tarjeta.java
@@ -0,0 +1,15 @@
+package com.ibm.academia.apirest.entities;
+
+import java.io.Serializable;
+
+import com.ibm.academia.apirest.enums.TipoTarjeta;
+
+
+public class Tarjeta implements Serializable {
+
+	private TipoTarjeta tipoTarjeta;
+	
+	
+	private static final long serialVersionUID = 2911762464333103201L;
+
+}
---
+++ Entregable/src/main/java/com/ibm/academia/apirest/enums/Preferencia.java
@@ -0,0 +1,10 @@
+package com.ibm.academia.apirest.enums;
+
+public enum Preferencia {
+	SHOPPING,
+	TRAVELS,
+	HELP,
+	MYBUSSINES,
+	SPORTS,
+	MYSTYLE
+}
